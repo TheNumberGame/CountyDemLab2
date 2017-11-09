e086740d6b246fddba333f403b038d8cf022ed16
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2017-11-09 10:57:45 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -24,7 +24,7 @@ def get_state_options():
         if not c["State"] == state:
             options += Markup("<option {% if {{ stateName }} ==\"" + c["State"]  +"\" %} selected {% endif %} value=\"" + c["State"] + "\">" + c["State"] + "</option>")
             state = c["State"]
-            println(options)
+            print(options)
     
     return options
 
